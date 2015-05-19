46446b7cb8ce97b01def1daa0d463536fb7a1165
jgem12345 <Megaman12345>
2015-05-18 22:59:27 -0500

    nc

--- js/inventory_parse.js
+++ js/inventory_parse.js
@@ -1,54 +1,10 @@
-<<<<<<< HEAD
-Parse.initialize("PRZCDqiKSpzjNuIGTEHj9jXKn6f1PRfAixB2nK2r",
-    "GuD81fbE4prg1RdLLmJvhLdb8CBa21imyroGrMRk");    
-
-var Hardware = Parse.Object.extend("Hardware");
-=======
  Parse.initialize("PRZCDqiKSpzjNuIGTEHj9jXKn6f1PRfAixB2nK2r", "GuD81fbE4prg1RdLLmJvhLdb8CBa21imyroGrMRk");    
 var Hardware = Parse.Object.extend("HW");
->>>>>>> 251256786932f7bbda582f3d5a79a5f3e747e120
 var query = new Parse.Query(Hardware);
 query.ascending("Name");
 query.find({
   success: function(results) {
     // Successfully retrieved the object.
-<<<<<<< HEAD
-    $("#hovertable").append(
-        "<tr><td></td>"+
-        "<td>"+
-        "<u><b>Name</b></u>"+
-        "</td>"+
-        "<td>"+
-        "<u><b>Condition</b></u>"+
-        "</td>"+
-        "<td>"+
-        "<u><b>Available</b></u>"+
-        "</td>"+
-        "</tr>");
-    // for loop
-    var count = 1;
-    for(O in results){
-      var t = results[O].get('Name');
-      var r = results[O].get('Condition');
-      var p = results[O].get('rented');
-      if(results[O].get('rented')){
-        p="no";
-      }
-      else{ p="yes";
-      }
-      $("#hovertable").append(
-          "<tr class=element><td>"+
-          (count)+
-          "</td><td>"+
-          t+
-          "</td><td>"+
-          r+
-          "</td><td>"+
-          p+
-          "</td></tr>");
-      count++;	
-    }
-=======
 	$("#hovertable").append(
           "<tr><td></td>"+
           "<td>"+
@@ -81,7 +37,6 @@ var count = 1;
             "</td></tr>");
         count++;	
 	}
->>>>>>> 251256786932f7bbda582f3d5a79a5f3e747e120
   },
   error: function(error) {
     alert("Error: " + error.code + " " + error.message);
@@ -91,6 +46,8 @@ var count = 1;
 
 function checkout(){
   
+
   alert("asdfsdf");
+
   
 }
